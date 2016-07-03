62ecff61c7e385ef9327ab753286490e4ed4e9a0
soyuka <soyuka@gmail.com>
2016-07-03 12:15:08 +0200

    v1.0.6

---
+++ dist/fclone.js
@@ -0,0 +1,67 @@
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD
+        define('fclone', [], factory);
+    } else if (typeof module === 'object' && module.exports) {
+			  //node
+        module.exports = factory();
+    } else {
+        // Browser globals (root is window)
+        root.fclone = factory();
+    }
+}(this, function () {
+  'use strict';
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
+
+function fclone(obj, refs) {
+  if (!obj || "object" !== (typeof obj === 'undefined' ? 'undefined' : _typeof(obj))) return obj;
+
+  if (obj instanceof Date) {
+    return new Date(obj);
+  }
+
+  if (typeof Buffer !== 'undefined' && Buffer.isBuffer(obj)) {
+    return new Buffer(obj);
+  }
+
+  if (!refs) {
+    refs = [];
+  }
+
+  if (Array.isArray(obj)) {
+    refs[refs.length] = obj;
+    var _l = obj.length;
+    var i = -1;
+    var _copy = [];
+
+    while (_l > ++i) {
+      _copy[i] = ~refs.indexOf(obj[i]) ? '[Circular]' : fclone(obj[i], refs);
+    }
+
+    refs.length && refs.length--;
+    return _copy;
+  }
+
+  refs[refs.length] = obj;
+  var copy = {};
+
+  if (obj instanceof Error) {
+    copy.name = obj.name;
+    copy.message = obj.message;
+    copy.stack = obj.stack;
+  }
+
+  var keys = Object.keys(obj);
+  var l = keys.length;
+
+  while (l--) {
+    var k = keys[l];
+    copy[k] = ~refs.indexOf(obj[k]) ? '[Circular]' : fclone(obj[k], refs);
+  }
+
+  refs.length && refs.length--;
+  return copy;
+}
+  return fclone
+}));
